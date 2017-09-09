fe9ab7c0abbc3feb981781160f3a6bc623a615da
Dustin Schau <dustinschau@gmail.com>
2017-09-09 10:54:42 -0500

    chore: rename imports

--- src/constants/snippets.ts
+++ src/constants/snippets.ts
@@ -1,9 +1,9 @@
 import * as StyledComponents from '../snippets/styled-components';
 import * as Glamorous from '../snippets/glamorous';
 import * as Aphrodite from '../snippets/aphrodite';
-import * as Cxs from '../snippets/cxs';
-import * as Radium from '../snippets/radium';
 import * as ReactJSS from '../snippets/react-jss';
 import * as JSS from '../snippets/jss';
+import * as CXS from '../snippets/cxs';
+import * as Radium from '../snippets/radium';
 
-export { StyledComponents, Glamorous, Aphrodite, Cxs, Radium, ReactJSS, JSS };
+export { StyledComponents, Glamorous, Aphrodite, ReactJSS, JSS, CXS, Radium };
