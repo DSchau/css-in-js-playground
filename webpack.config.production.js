993e1f537f54425f56ca9b9448bf751a2a5e3450
Dustin Schau <DSchau@users.noreply.github.com>
2017-09-10 13:53:28 -0500

    ci: set up s3 deploy (#35)
    * ci: set up s3 deploy
    
    * chore: set acl to public
    
    * build: add compression plugin

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OfflinePlugin = require('offline-plugin');
 const NameAllModulesPlugin = require('name-all-modules-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const { name } = require('./package.json');
 const isNetlify = typeof process.env.CONTEXT !== 'undefined';
@@ -44,6 +45,11 @@ module.exports = {
       ServiceWorker: {
         events: true
       }
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js(on)?|html|css)$/
     })
   ]
 };
